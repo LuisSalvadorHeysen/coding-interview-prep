31982e88bec4161ccbb7a9717718c9837b469f93
LuisHeysen <lsalvador.ht@gmail.com>
2026-08-06 00:59:35 -0700

    iterative dp

--- Longest_Palindromic_Subsequence/Longest_Palindromic_Subsequence.cpp
+++ Longest_Palindromic_Subsequence/Longest_Palindromic_Subsequence.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    int go(const string &s, int l, int r, vector<vector<int>> &memo) {
-        if (l > r) {
-            return 0;
-        }
-
-        if (l == r) {
-            return 1;
-        }
-
-        if (memo[l][r] != -1) {
-            return memo[l][r];
-        }
-
-        if (s[l] == s[r]) {
-            return memo[l][r] = 2 + go(s, l + 1, r - 1, memo);
-        } else {
-            return memo[l][r] = max(go(s, l + 1, r, memo), go(s, l, r - 1, memo));
-        }
-    }
-
     int longestPalindromeSubseq(string s) {
         int n = int(s.size());
 
-        vector<vector<int>> memo(n, vector<int>(n, -1));
-        return go(s, 0, n - 1, memo);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i; j < n; ++j) {
+                if (j == i) {
+                    dp[i][j] = 1;
+                } else if (s[i] == s[j]) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
     }
 };
